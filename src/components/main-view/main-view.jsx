91ea4df68503ec9cea51cb277396686d720a2d53
Meran Arumugam <meran.arumugam@gmail.com>
2023-06-21 13:20:33 -0400

    Update main-view.jsx
    updated code and changed to lower case

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,12 +5,25 @@ import { MovieView } from "../movie-view/movie-view";
 export const MainView = () => {
     const [movies, setMovies] = useState([
         {
-            id:1,
-            Title: "Avengers:Endgame",
-            Description: "Avengers assemble once more to defeat Thanos and to restore the Universe and balance to the world.",
-            Genre: "Action",
-            Director: "Anthony Russo",
-            Image: "https://www.google.com/search?rlz=1C5CHFA_enCA913CA913&sxsrf=APwXEde_XHWZ-sTNQL0e2C_yzJJo7uEBkw:1687363050549&q=avengers+endgame&tbm=isch&sa=X&ved=2ahUKEwj2tfCZ3dT_AhV0IH0KHSEsA_AQ0pQJegQIDRAB&biw=1440&bih=702&dpr=2#imgrc=4K2BvVWQhfuweM"
+            id: 1,
+            title: "Avengers:Endgame",
+            description: "Avengers assemble once more to defeat Thanos and to restore the Universe and balance to the world.",
+            genre: "Action",
+            director: "Anthony Russo",
+    },
+    {
+        id: 2,
+        title: "The Dark Knight",
+        description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
+        genre: "Action",
+        director: "Christopher Nolan",
+    },
+    {
+        id: 3,
+        title: "Spider-Man: No way Home",
+        description: "Spider man has got his identity revealed and got help from Doctor Strange. A spell went wrong and dangerous enemies from other worlds start to appear where Peter learns what it means to be Spider-Man.",
+        genre: "Action",
+        director: "Jon Watts"
     }
 ]);
 
