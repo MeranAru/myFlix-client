8bcdc78e20f77192d6832c4576b0d12221239d59
Meran Arumugam <meran.arumugam@gmail.com>
2023-06-28 17:52:26 -0400

    Update main-view.jsx
    added useEffect

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,31 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-    const [movies, setMovies] = useState([
-        {
-            id: 1,
-            title: "Avengers:Endgame",
-            description: "Avengers assemble once more to defeat Thanos and to restore the Universe and balance to the world.",
-            genre: "Action",
-            director: "Anthony Russo", 
-    },
-    {
-        id: 2,
-        title: "The Dark Knight",
-        description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-        genre: "Action",
-        director: "Christopher Nolan",
-    },
-    {
-        id: 3,
-        title: "Spider-Man: No way Home",
-        description: "Spider man has got his identity revealed and got help from Doctor Strange. A spell went wrong and dangerous enemies from other worlds start to appear where Peter learns what it means to be Spider-Man.",
-        genre: "Action",
-        director: "Jon Watts"
-    }
-]);
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        fetch("https://git.heroku.com/movie-api-meran.git")
+            .then((response) => response.json())
+            .then((data) => {
+                const moviesFromApi = data.docs.map((doc) => {
+                    return {
+                        id: doc.key,
+                        title: doc.title,
+                        description: doc.desciption,
+                        genre: doc.genre,
+                        director: doc.director_name?.[0]
+                    };
+                });
+                setMovies(moviesFromApi);
+                console.log("movies from api", data);
+                });
+            }, []);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
 
