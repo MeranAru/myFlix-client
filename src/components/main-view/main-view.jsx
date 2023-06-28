46698f860c9f7a46a11816197e6bbeec59543a1c
Meran Arumugam <meran.arumugam@gmail.com>
2023-06-28 17:43:21 -0400

    Update main-view.jsx
    Corrected coding errors

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -6,11 +6,11 @@ export const MainView = () => {
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
-        fetch("mongodb+srv://meranarumugam:12345@movieapi.0isxclf.mongodb.net/myFlixDB?retryWrites=true&w=majority")
+        fetch("https://git.heroku.com/movie-api-meran.git")
             .then((response) => response.json())
             .then((data) => {
                 const moviesFromApi = data.docs.map((doc) => {
-                    return{
+                    return {
                         id: doc.key,
                         title: doc.title,
                         description: doc.description,
@@ -18,10 +18,10 @@ export const MainView = () => {
                         director: doc.director_name?.[0]
                     };
                 });
-                setMovies(booksFromApi);
-                console.log(" movies from api", data);
-            });
-    }, []);
+                setMovies(moviesFromApi);
+                console.log("movies from api", data);
+                });
+            }, []);
 
     const [selectedMovie, setSelectedMovie] = useState(null);
 
