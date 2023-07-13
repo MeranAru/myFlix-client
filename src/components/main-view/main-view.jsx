8cba0884a1d3b6571ee73acbd75ee537459e97d1
Meran Arumugam <meran.arumugam@gmail.com>
2023-07-12 20:59:47 -0400

    JSX files
    imported scss to login, main and movie card files

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,6 +5,7 @@ import { LoginView } from "../login-view/login-view";
 import { SignupView } from "../signup-view/signup-view";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import "./main-view.scss";
 
 export const MainView = () => {
     const [movies, setMovies] = useState([]);
