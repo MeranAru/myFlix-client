692d09f64f6f059610e026649263c00c2f8827e4
Meran Arumugam <meran.arumugam@gmail.com>
2023-12-22 08:06:42 -0500

    Update signup-view.jsx
    updated code

--- src/components/signup-view/signup-view.jsx
+++ src/components/signup-view/signup-view.jsx
@@ -1,12 +1,12 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 
 export const SignupView = () => {
-    const [ username, setUsername ] = useState("");
-    const [ password, setPassword ] = useState("");
-    const [ email, setEmail ] = useState("");
-    const [ birthday, setBirthday ] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+    const [birthday, setBirthday] = useState("");
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -15,66 +15,63 @@ export const SignupView = () => {
             Username: username,
             Password: password,
             Email: email,
-            Birthday: birthday
+            BirthDate: birthday
         };
 
-        fetch("https://movie-api-meran.herokuapp.com/users.json", {
-            method:"POST",
+        fetch("https://movie-api2-7e3e05174777.herokuapp.com/users", {
+            method: "POST",
             body: JSON.stringify(data),
-            headers:{
-                "Content-Type":"application/json"
+            headers: {
+                "Content-Type": "application/json"
             }
         }).then((response) => {
             if (response.ok) {
-                alert("Signup Successful");
-                window.location.reload();
+                alert("Signup successful!");
+                //window.location.reload();
             } else {
-                alert("Signup failed");
-            };
+                alert("Signup failed!");
+            }
         });
     };
 
     return (
-        <Form onSubmit= {handleSubmit}>
-            <Form.Group controlId="signUpFormUsername">
+        <Form onSubmit={handleSubmit} className="text-white">
+            <Form.Group controlId="formUsername">
                 <Form.Label>Username:</Form.Label>
-                <Form.Control 
-                    type="text"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    required
-                    minLength="3"
+                <Form.Control
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+                minLength="5"
                 />
             </Form.Group>
-
-            <Form.Group controlId="signUpFormPassword">
+            <Form.Group controlId="formPassword">
                 <Form.Label>Password:</Form.Label>
-                <Form.Control 
-                    type="password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    required
+                <Form.Control
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+                minLength="5"
                 />
             </Form.Group>
-
-            <Form.Group controlId="signUpFormEmail">
+            <Form.Group controlId="formEmail">
                 <Form.Label>Email:</Form.Label>
-                <Form.Control 
-                    type="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    required
+                <Form.Control
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
                 />
             </Form.Group>
-
-
-            <Form.Group controlId="signUpFormBirthday">
+            <Form.Group controlId="formBirthday">
                 <Form.Label>Birthday:</Form.Label>
-                <Form.Control 
-                    type="date"
-                    value={birthday}
-                    onChange={(e) => setBirthday(e.target.value)}
-                    required
+                <Form.Control
+                type="date"
+                value={birthday}
+                onChange={(e) => setBirthday(e.target.value)}
+                required
                 />
             </Form.Group>
             <Button variant="primary" type="submit">Submit</Button>
