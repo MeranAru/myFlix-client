9eede5e847aacb54e916fd911b13da9f59c9d76f
Meran Arumugam <meran.arumugam@gmail.com>
2023-12-22 08:06:15 -0500

    Update profile-view.jsx
    updated code

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,48 +1,135 @@
-import React, { useEffect, useState } from "react";
-import { Container, Col, Row, Card } from "react-bootstrap";
-import { Link } from "react-router-dom";
-import UserInfo from "./user-info";
-import FavoriteMovies from "./favorite-movies";
-import UpdateUser from "./update-user";
-import "./profile-view.scss";
+import { useState, useEffect } from "react";
+import Button from "react-bootstrap/Button";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Modal from "react-bootstrap/Modal";
+import { MovieCard } from "../movie-card/movie-card";
+import { ModalHeader } from "react-bootstrap";
 
-export function ProfileView({ movies, onUpdatedUserInfo }) {
-    const [ user, setUser ] = useState({
-        Username: "",
-        Email: "",
-        FavoriteMovies:[],
-    });
-    const favoriteMovieList = movies.filter((movies) => {
-        return user.FavoriteMovies.includes(movies._id);
-    });
+export const ProfileView = ({ user, token, setUser, movies, onLogout }) => {
+    const [username, setUsername] = useState(user.Username);
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState(user.Email);
+    const [birthday, setBirthday] = useState(user.BirthDate);
+    const [showModal, setShowModal] = useState(false);
 
-    useEffect(() => {
-        let isMounted = true;
-        isMounted && getUser();
-        return () => {
-            isMounted = false;
-        }
-    }, [])
+    const handleShowModal = () => setShowModal(true);
+    const handleCloseModal = () => setShowModal(false);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const data = {
+            Username: username,
+            Password: password,
+            Email: email,
+            BirthDate: birthday
+        };
+
+        fetch(`https://movie-api2-7e3e05174777.herokuapp.com/users/${user.Username}`, {
+            method: "PUT",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response) => {
+            if (response.ok) {
+                return response.json()
+            } else {
+                alert("Update failed.")
+            }
+        }).then((data) => {
+            if (data) {
+                localStorage.setItem("user", JSON.stringify(data));
+                setUser(data);
+            }
+        })
+    };
+
+    const handleDeleteUser = () => {
+        fetch(`https://movie-api2-7e3e05174777.herokuapp.com/users/${user.Username}`, {
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }).then((response) => {
+            if (response.ok) {
+                onLogout();
+            } else {
+                alert("something went wrong.")
+            }
+        })
+    }
 
     return (
-        <Container>
-            <Row>
-                <Col xs={12} sm={4}>
-                    <Card>
-                        <Card.Body>
-                            <UserInfo name={user.Username} email={user.Email} />
-                        </Card.Body>
-                    </Card>
-                </Col>
-                <Col xs={12} sm={8}>
-                    <Card>
-                        <Card.Body>
-                            <UpdateUser user={user} setUser={setUser} />
-                        </Card.Body>
-                    </Card>
-                </Col>
-            </Row>
-            <FavoriteMovies favoriteMovieList={favoriteMovieList} />
-        </Container>
+        <>
+        <h1 className="text-white">Profile</h1>
+        <Row>
+            <Col className="text-white">
+                <h3 className="text-white">Your profile details</h3>
+                <div>Username: {user.Username}</div>
+                <div>Email: {user.Email}</div>
+            </Col>
+            <Col>
+            <h3 className="text-white">Update your profile information here.</h3>
+            <Form onSubmit={handleSubmit} className="text-white">
+                <Form.Group controlId="formUsername">
+                    <Form.Label>Username:</Form.Label>
+                    <Form.Control
+                        type="text"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                        minLength="5" 
+                    />
+                </Form.Group>
+                <Form.Group controlId="formPassword">
+                    <Form.Label>Password:</Form.Label>
+                    <Form.Control
+                        type="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                        minLength="5"
+                    />
+                </Form.Group>
+                <Form.Group controlId="formEmail">
+                    <Form.Label>Email:</Form.Label>
+                    <Form.Control
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
+                </Form.Group>
+                <Form.Group controlId="formBirthday">
+                    <Form.Label>Birthday:</Form.Label>
+                    <Form.Control
+                        type="date"
+                        value={birthday}
+                        onChange={(e) => setBirthday(e.target.value)}
+                        required
+                    />
+                </Form.Group>
+                <Button variant="primary" type="submit">Save changes</Button>
+            </Form>
+            </Col>
+        </Row>
+        <Button variant="primary" onClick={handleShowModal}>
+            Delete my account
+        </Button>
+        <Modal show={showModal} onHide={handleCloseModal}>
+            <Modal.Header closeButton>
+                <Modal.Title>Delete account</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Are you sure you want to delete your account permanantly?</Modal.Body>
+            <Modal.Footer>
+                <Button variant="primary" onClick={handleDeleteUser}>Yes</Button>
+                <Button variant="secondary" onClick={handleCloseModal}>No</Button>
+            </Modal.Footer>
+        </Modal>
+        </>
     )
 }
