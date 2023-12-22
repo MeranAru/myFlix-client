79e357bbdaeb23428f85eea4664fa8ceb20376c9
Meran Arumugam <meran.arumugam@gmail.com>
2023-12-22 08:11:17 -0500

    Update login-view.jsx
    updated code

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,52 +1,61 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import "./login-view.scss";
 
 export const LoginView = ({ onLoggedIn }) => {
-    const [ username, setUsername ] = useState("");
-    const [ password, setPassword ] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
 
     const handleSubmit = (event) => {
-// this prevents the default behavior of the form which is to reload the entire page
         event.preventDefault();
 
         const data = {
-            "Username": username,
-            "Password": password
+            Username: username,
+            Password: password 
         };
-        console.log(data)
-        fetch("https://movie-api-meran.herokuapp.com/login?Username="+username+"&Password="+password, {
-            method: "POST"
-        }).then((response) => {
-            if (response.ok) {
-                onLoggedIn(username);
+
+        fetch(`https://movie-api2-7e3e05174777.herokuapp.com/login?` + new URLSearchParams(data), {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            //body: JSON.stringify(data)
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log("Login response: ", data);
+            if (data.user) {
+                localStorage.setItem("user", JSON.stringify(data.user));
+                localStorage.setItem("token", data.token);
+                onLoggedIn(data.user, data.token);
             } else {
-                alert("Login Failed");
+                alert("No such user found.");
             }
+        })
+        .catch((e) => {
+            alert("Something went wrong");
         });
     };
 
     return (
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit} className="text-white">
             <Form.Group controlId="formUsername">
-                <Form.Label>Username:</Form.Label>
+                <Form.Label>Username: </Form.Label>
                 <Form.Control
-                    type= "text"
-                    value= {username}
-                    onChange= {(e) => setUsername(e.target.value)}
-                    required
-                    minLength= "3"
+                type="text"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
                 />
+            
             </Form.Group>
-
             <Form.Group controlId="formPassword">
-                <Form.Label>Password:</Form.Label>
+                <Form.Label>Password: </Form.Label>
                 <Form.Control
-                    type="password"
-                    value= {password}
-                    onChange= {(e) => setPassword(e.target.value)}
-                    required
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
                 />
             </Form.Group>
             <Button variant="primary" type="submit">Submit</Button>
